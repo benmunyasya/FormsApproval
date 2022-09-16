6861733818795eef04db3de34ebf5c74b63cf76b
benngovi47 <benngovi47@gmail.com>
2022-09-16 14:05:48 +0300

    dashboardhome

--- WORKFLOW/settings.py
+++ WORKFLOW/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 1#os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = [ 'localhost','127.0.0.1','172.16.1.9','VIRTUAL.kws.local']
+ALLOWED_HOSTS = ['*','localhost','127.0.0.1','172.16.1.9','VIRTUAL.kws.local','172.16.4.160']
 #SESSION_COOKIE_SECURE=True
 #CSRF_COOKIE_SECURE=True
 
